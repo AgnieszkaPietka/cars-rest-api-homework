681b06a252448ada1dba3a7e8afb034391bc4f39
Agnieszka <gnostka@gmail.com>
2021-02-04 22:02:47 +0100

    Basic project structure

---
+++ src/main/java/pl/bykowski/carsrestapi/CarsRestApiApplication.java
@@ -0,0 +1,13 @@
+package pl.bykowski.carsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bykowski/carsrestapi/CarsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bykowski.carsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
